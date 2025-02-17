f727f350019bdd1ae11d7f68897e2a95b753cc7b
Meshari <mehary506@gmail.com>
2025-02-17 09:44:45 +0000

    T

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -33,38 +33,3 @@ int main(void)
 
 	return (0);
 }
-tdio.h>
-
-/**
- * main - Entry point of the program
- *
- * Description: This program prints all possible different combinations
- * of two digits in ascending order, separated by ", ".
- * The two digits must be different, and the smallest combination is printed.
- * It only uses putchar and does not declare char variables.
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-  int digit1, digit2;
-
-  for (digit1 = 0; digit1 < 9; digit1++)
-    {
-      for (digit2 = digit1 + 1; digit2 < 10; digit2++)
-	{
-	  putchar(digit1 + '0');
-	  putchar(digit2 + '0');
-
-	  if (digit1 != 8 || digit2 != 9)
-	    {
-	      putchar(',');
-	      putchar(' ');
-	    }
-	}
-    }
-
-  putchar('\n');
-
-  return (0);
-  
