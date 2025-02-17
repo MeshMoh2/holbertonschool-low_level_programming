d963289ca93d45588481d001380d5b2f3383164c
Meshari <mehary506@gmail.com>
2025-02-17 10:30:40 +0000

    T

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,17 +1,17 @@
 #include <unistd.h>
 
 /**
- * main - Entry point of the program
+ * main - Entry point
  *
- * Description: This program prints the specified quote to standard error
- * without using printf or puts. It returns 1 as required.
+ * Description: Prints the given quote to standard error
+ * without using printf or puts.
  *
  * Return: Always 1 (Failure)
  */
 int main(void)
 {
-	write(2,
-	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	      59);
-	return (1);
+    write(2,
+          "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+          59);
+    return (1);
 }
