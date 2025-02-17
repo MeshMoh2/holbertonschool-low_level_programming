d7739fcf5e9df798bb440ee7c82df9c06d899a5c
Meshari <mehary506@gmail.com>
2025-02-17 10:52:45 +0000

    T

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -11,9 +11,10 @@
  */
 int main(void)
 {
-	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	const char *message = "and that piece of art is useful\" - Dora Korpar, "
+			      "2015-10-19\n";
 
-	write(2, message, 59); /* 59 is the length of the message including the newline */
+	write(2, message, 59); /* 59 is the length of the message including newline */
 
 	return (1);
 }
