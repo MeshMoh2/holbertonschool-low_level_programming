f2cf2adbe1098fce85367850c2966b5d441f8d78
Meshari <mehary506@gmail.com>
2025-02-20 16:51:58 +0000

    T

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -46,4 +46,11 @@ int print_sign(int n);
  */
 int _abs(int n);
 
+/**
+ * print_last_digit - Prints the last digit of a number
+ * @n: The number to extract the last digit from
+ * Return: The value of the last digit
+ */
+int print_last_digit(int n);
+
 #endif /* MAIN_H */
