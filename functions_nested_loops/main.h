deaba44db563cff0d8a5ec1305131f524a960f70
Meshari <mehary506@gmail.com>
2025-02-20 16:16:14 +0000

    T

---
+++ functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - Writes a character to stdout
+ * @c: The character to print
+ * Return: 1 on success, -1 on error
+ */
+int _putchar(char c);
+
+#endif /* MAIN_H */
