76c73c4ebb27ec37df0be36181bd997f4a92cd5c
Meshari <mehary506@gmail.com>
2025-02-27 11:14:00 +0000

    T

--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - Computes and prints the sum of all the multiples of 3 or 5 below 1024.
+ * main - Computes and prints the sum of all the multiples
+ * of 3 or 5 below 1024.
  *
  * Description: Iterates through numbers from 0 to 1023,
  * checks if they are divisible by 3 or 5, and adds them to the sum.
