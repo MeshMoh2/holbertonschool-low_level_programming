05d1599db9f3e7733ac115345958c69d84c5bc57
Meshari <mehary506@gmail.com>
2025-03-03 18:41:38 +0000

    T

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -6,12 +6,11 @@
  */
 void _puts_recursion(char *s)
 {
-    if (*s == '\0')  /* Base case: if we reach the end of the string */
-    {
-        _putchar('\n');  /* Print a new line */
-        return;
-    }
-
-    _putchar(*s);  /* Print the current character */
-    _puts_recursion(s + 1);  /* Recursively call function with next character */
+	if (*s == '\0') /* Base case: if we reach the end of the string */
+	{
+		_putchar('\n'); /* Print a new line */
+		return;
+	}
+	_putchar(*s); /* Print the current character */
+	_puts_recursion(s + 1); /* Recursively call function with next character */
 }
