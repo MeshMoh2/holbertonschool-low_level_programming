a25d60c15313c3bc00f5d7f960272d567fe14b78
Your Name <you@example.com>
2025-03-21 14:53:54 +0000

    T

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -19,8 +19,12 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (ops[i].op != NULL && *(ops[i].op) != *s)
+	while (ops[i].op != NULL)
+	{
+		if (s[0] == ops[i].op[0] && s[1] == '\0')
+			return (ops[i].f);
 		i++;
+	}
 
-	return (ops[i].f);
+	return (NULL);
 }
