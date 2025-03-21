dc9071afc5108848c28c7f8bb92ad9fa5ef22082
Your Name <you@example.com>
2025-03-21 14:31:26 +0000

    T

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,17 +1,6 @@
-#include "function_pointers.h"
-#include <stddef.h>
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 
-/**
- * print_name - prints a name using a given function
- * @name: the name to print
- * @f: pointer to a function that takes a char * argument
- *
- * Return: Nothing.
- */
-void print_name(char *name, void (*f)(char *))
-{
-	if (name != NULL && f != NULL)
-	{
-		f(name);
-	}
-}
+void print_name(char *name, void (*f)(char *));
+
+#endif /* FUNCTION_POINTERS_H */
