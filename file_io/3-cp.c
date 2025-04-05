ae1a2082881f1cc4b2ce161fbe4d9219e888095b
Your Name <you@example.com>
2025-04-05 19:31:58 +0000

    T

---
+++ file_io/3-cp.c
@@ -0,0 +1,71 @@
+#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define BUF_SIZE 1024
+
+/**
+ * print_error - Prints an error message and exits with given code
+ * @code: Exit code
+ * @msg: Message format
+ * @arg: Argument to insert into message
+ */
+void print_error(int code, const char *msg, const char *arg)
+{
+	dprintf(STDERR_FILENO, msg, arg);
+	exit(code);
+}
+
+/**
+ * main - Copies the content of one file to another
+ * @ac: Argument count
+ * @av: Argument vector
+ *
+ * Return: 0 on success, exits with error code otherwise
+ */
+int main(int ac, char **av)
+{
+	int fd_from, fd_to, r_bytes, w_bytes;
+	char buffer[BUF_SIZE];
+
+	if (ac != 3)
+		print_error(97, "Usage: %s file_from file_to\n", av[0]);
+
+	fd_from = open(av[1], O_RDONLY);
+	if (fd_from == -1)
+		print_error(98, "Error: Can't read from file %s\n", av[1]);
+
+	fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		close(fd_from);
+		print_error(99, "Error: Can't write to %s\n", av[2]);
+	}
+
+	while ((r_bytes = read(fd_from, buffer, BUF_SIZE)) > 0)
+	{
+		w_bytes = write(fd_to, buffer, r_bytes);
+		if (w_bytes == -1 || w_bytes != r_bytes)
+		{
+			close(fd_from);
+			close(fd_to);
+			print_error(99, "Error: Can't write to %s\n", av[2]);
+		}
+	}
+
+	if (r_bytes == -1)
+	{
+		close(fd_from);
+		close(fd_to);
+		print_error(98, "Error: Can't read from file %s\n", av[1]);
+	}
+
+	if (close(fd_from) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
+	if (close(fd_to) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
+
+	return (0);
+}
