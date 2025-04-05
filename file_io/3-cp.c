73dd4611af32387f00ad56fd2a61e265e825855f
Your Name <you@example.com>
2025-04-05 19:52:23 +0000

    T

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -8,26 +8,27 @@
 
 /**
  * print_error - Prints an error message to stderr and exits
- * @exit_code: Exit status code
- * @format: Format string for dprintf
- * @arg: Argument to include in message
+ * @code: The exit code
+ * @message: The format string
+ * @arg: The argument to insert in format
  */
-void print_error(int exit_code, const char *format, const char *arg)
+void print_error(int code, const char *message, const char *arg)
 {
-	dprintf(STDERR_FILENO, format, arg);
-	exit(exit_code);
+	dprintf(STDERR_FILENO, message, arg);
+	exit(code);
 }
 
 /**
- * main - Copies content from one file to another
+ * main - Copies content of one file to another
  * @ac: Argument count
  * @av: Argument vector
  *
- * Return: 0 on success, exits with codes on error
+ * Return: 0 on success, otherwise exits with appropriate code
  */
 int main(int ac, char **av)
 {
-	int fd_from, fd_to, r_bytes, w_bytes;
+	int fd_from, fd_to;
+	ssize_t r_bytes, w_bytes;
 	char buffer[BUFFER_SIZE];
 
 	if (ac != 3)
@@ -47,19 +48,15 @@ int main(int ac, char **av)
 		print_error(99, "Error: Can't write to %s\n", av[2]);
 	}
 
-	while (1)
+	/* Read loop */
+	while ((r_bytes = read(fd_from, buffer, BUFFER_SIZE)) != 0)
 	{
-		r_bytes = read(fd_from, buffer, BUFFER_SIZE);
-
 		if (r_bytes == -1)
 		{
 			close(fd_from);
 			close(fd_to);
 			print_error(98, "Error: Can't read from file %s\n", av[1]);
 		}
-
-		if (r_bytes == 0)
-			break;
 
 		w_bytes = write(fd_to, buffer, r_bytes);
 		if (w_bytes == -1 || w_bytes != r_bytes)
@@ -75,7 +72,6 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
 		exit(100);
 	}
-
 	if (close(fd_to) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
