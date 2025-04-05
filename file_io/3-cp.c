bd34b9453cd358190e28f12b65bd61072b58ae13
Your Name <you@example.com>
2025-04-05 19:56:47 +0000

    T

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -6,11 +6,17 @@
 
 #define BUFFER_SIZE 1024
 
+void print_error(int code, const char *format, const char *arg);
+void check_arguments(int ac);
+int open_source_file(const char *file);
+void close_file(int fd);
+void read_and_copy(int fd_from, const char *file_to, const char *file_from);
+
 /**
- * print_error - Prints error message and exits
+ * print_error - Prints an error message and exits
  * @code: Exit code
  * @format: Error message format
- * @arg: Error message argument
+ * @arg: Argument for format string
  */
 void print_error(int code, const char *format, const char *arg)
 {
@@ -19,41 +25,71 @@ void print_error(int code, const char *format, const char *arg)
 }
 
 /**
- * main - Copies the content of a file to another file
+ * check_arguments - Validates argument count
  * @ac: Argument count
- * @av: Argument vector
- *
- * Return: 0 on success, exits with error code otherwise
  */
-int main(int ac, char **av)
+void check_arguments(int ac)
 {
-	int fd_from, fd_to;
-	ssize_t r_bytes, w_bytes;
-	char buffer[BUFFER_SIZE];
-
 	if (ac != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
+}
 
-	fd_from = open(av[1], O_RDONLY);
-	if (fd_from == -1)
-		print_error(98, "Error: Can't read from file %s\n", av[1]);
+/**
+ * open_source_file - Opens file_from safely
+ * @file: Name of the source file
+ *
+ * Return: File descriptor of source file
+ */
+int open_source_file(const char *file)
+{
+	int fd = open(file, O_RDONLY);
 
-	/* First read BEFORE opening file_to */
+	if (fd == -1)
+		print_error(98, "Error: Can't read from file %s\n", file);
+
+	return (fd);
+}
+
+/**
+ * close_file - Closes a file descriptor with error checking
+ * @fd: File descriptor to close
+ */
+void close_file(int fd)
+{
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * read_and_copy - Reads from file_from and writes to file_to
+ * @fd_from: Source file descriptor
+ * @file_to: Name of the destination file
+ * @file_from: Name of the source file (for error messages)
+ */
+void read_and_copy(int fd_from, const char *file_to, const char *file_from)
+{
+	int fd_to;
+	ssize_t r_bytes, w_bytes;
+	char buffer[BUFFER_SIZE];
+
 	r_bytes = read(fd_from, buffer, BUFFER_SIZE);
 	if (r_bytes == -1)
 	{
-		close(fd_from);
-		print_error(98, "Error: Can't read from file %s\n", av[1]);
+		close_file(fd_from);
+		print_error(98, "Error: Can't read from file %s\n", file_from);
 	}
 
-	fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
-		close(fd_from);
-		print_error(99, "Error: Can't write to %s\n", av[2]);
+		close_file(fd_from);
+		print_error(99, "Error: Can't write to %s\n", file_to);
 	}
 
 	while (r_bytes > 0)
@@ -61,31 +97,38 @@ int main(int ac, char **av)
 		w_bytes = write(fd_to, buffer, r_bytes);
 		if (w_bytes == -1 || w_bytes != r_bytes)
 		{
-			close(fd_from);
-			close(fd_to);
-			print_error(99, "Error: Can't write to %s\n", av[2]);
+			close_file(fd_from);
+			close_file(fd_to);
+			print_error(99, "Error: Can't write to %s\n", file_to);
 		}
 
 		r_bytes = read(fd_from, buffer, BUFFER_SIZE);
 		if (r_bytes == -1)
 		{
-			close(fd_from);
-			close(fd_to);
-			print_error(98, "Error: Can't read from file %s\n", av[1]);
+			close_file(fd_from);
+			close_file(fd_to);
+			print_error(98, "Error: Can't read from file %s\n", file_from);
 		}
 	}
 
-	if (close(fd_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
-	}
+	close_file(fd_from);
+	close_file(fd_to);
+}
 
-	if (close(fd_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
+/**
+ * main - Entry point
+ * @ac: Argument count
+ * @av: Argument vector
+ *
+ * Return: 0 on success
+ */
+int main(int ac, char **av)
+{
+	int fd_from;
+
+	check_arguments(ac);
+	fd_from = open_source_file(av[1]);
+	read_and_copy(fd_from, av[2], av[1]);
 
 	return (0);
 }
