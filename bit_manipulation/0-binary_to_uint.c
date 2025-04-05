f66c222159b72f122a4a9764e6f2a043849f4893
Your Name <you@example.com>
2025-04-05 19:00:52 +0000

    T

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include <stddef.h>  /* for NULL */
+#include "main.h"
+
+/**
+ * binary_to_uint - Converts a binary string to an unsigned int
+ * @b: Pointer to a string of 0 and 1 chars
+ *
+ * Return: The converted number, or 0 if b is NULL or contains invalid chars
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (*b)
+	{
+		if (*b != '0' && *b != '1')
+			return (0);
+
+		result = result * 2 + (*b - '0');
+		b++;
+	}
+
+	return (result);
+}
